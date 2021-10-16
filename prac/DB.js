696cdf2ab21456222944e3179b41f9bcc10c7a09
PositivePeriod <positiveperiod.dev@gmail.com>
2021-10-17 06:15:11 +0900

    feat: hotfix for security

--- prac/DB.js
+++ prac/DB.js
@@ -1,15 +1,19 @@
+require('dotenv').config();
+
 const { PostgresqlDB, LocalDB } = require("../game/database");
 
 var DBManager = new LocalDB();
 var post = new PostgresqlDB();
 var local = new LocalDB();
+const devID = process.env.DEVELOP_PLAYERID;
+
 
 function singletonCheck() {
     console.log(new PostgresqlDB() === new PostgresqlDB());
 }
 
 async function test1() {
-    var nameList = ['qwe', '1234', 'DEVELOPER', 'ewfrgty7eej5hs4535hw6j']
+    var nameList = ['qwe', '1234', 'DEVELOPER', 'qwdqdwdq']
     for (const name of nameList) {
         var a = await DBManager.existName(name);
         console.log(name, a);
@@ -30,11 +34,11 @@ async function test2() {
     await DBManager.end();
     // console.log(DBManager.pool.ended);
     console.log(await DBManager.getPlayerByName('DEVELOPER')); // player object
-    console.log(await DBManager.getPlayerByID('AKCOviCah32K7pJKbMimL')); // player object
+    console.log(await DBManager.getPlayerByID(devID)); // player object
     console.log(await DBManager.getPlayerByName('lp')); // null
     console.log(await DBManager.getPlayerByID('lp')); // null
     console.log(await DBManager.existName('DEVELOPER')); // true
-    console.log(await DBManager.existName('AKCOviCah32K7pJKbMimL')); // false
+    console.log(await DBManager.existName(devID)); // false
 }
 
 async function test3() {
@@ -49,16 +53,16 @@ async function test4() {
     // console.log(d.data.join_date, typeof d.data.join_date);
     var playerInfo = { playerName: 'client2', playerID: 'examplkokoeid' }
     console.log(await local.setPlayer(playerInfo));
-    var playerInfo = { playerName: 'DEVELOPER', playerID: 'AKCOviCah32K7pJKbMimL' }
+    var playerInfo = { playerName: 'DEVELOPER', playerID: devID }
     console.log(await local.setPlayer(playerInfo));
     var d = await local.getPlayerByName('client2');
     console.log(d.data.join_date, typeof d.data.join_date);
     console.log(await DBManager.getPlayerByName('DEVELOPER'));
-    console.log(await DBManager.getPlayerByID('AKCOviCah32K7pJKbMimL'));
+    console.log(await DBManager.getPlayerByID(devID));
     console.log(await DBManager.getPlayerByName('lp'));
     console.log(await DBManager.getPlayerByID('lp'));
     console.log(await DBManager.existName('DEVELOPER')); // t
-    console.log(await DBManager.existName('AKCOviCah32K7pJKbMimL'));
+    console.log(await DBManager.existName(devID));
     console.log(await DBManager.getPlayerByName('DEVELOPER'));
 }
 
